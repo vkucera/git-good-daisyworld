bd41f7fc4df502603d6c4b54ada2f7e3026a14c0
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2024-04-03 12:29:04 +0200

    Add Cmake configuration

---
+++ build/CMakeFiles/daisyworld.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/srossi/icsc/git-good/daisy.cpp" "CMakeFiles/daisyworld.dir/daisy.cpp.o" "gcc" "CMakeFiles/daisyworld.dir/daisy.cpp.o.d"
+  "/home/srossi/icsc/git-good/main.cpp" "CMakeFiles/daisyworld.dir/main.cpp.o" "gcc" "CMakeFiles/daisyworld.dir/main.cpp.o.d"
+  "/home/srossi/icsc/git-good/world.cpp" "CMakeFiles/daisyworld.dir/world.cpp.o" "gcc" "CMakeFiles/daisyworld.dir/world.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/daisyworld.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for daisyworld.
+# This may be replaced when dependencies are built.
