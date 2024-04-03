d9c7eafce947bc5dedf32ed619efa42ec752c155
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2024-04-03 14:27:32 +0200

    Add print to main function
    Add useful comment

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include "world.hpp"
 
-#include <chrono>
 #include <iostream>
-#include <thread>
 
 int main() {
   int size{20};
@@ -12,15 +10,14 @@ int main() {
   World world(size, start_black_percentage, start_white_percentage, max_age);
   std::cout << "Initial global temperature: " << world.global_temperature()
             << '\n';
-  int it{0};
-  int num_iterations{100};
-  while (it++ < num_iterations) {
-    std::cout << "\033c";
-    world.step(1.);
-    std::cout << "Global temperature after " << it
-              << " time step(s): " << world.global_temperature() << '\n';
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  }
-  std::cout << '\n';
+  world.print();
+
+  std::cin.get();
+  std::cout << "\033c"; // Clear the terminal
+
+  world.step(1.);
+  std::cout << "Global temperature after one time step: "
+            << world.global_temperature() << '\n';
+  world.print();
   return 0;
 }
